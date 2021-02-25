bb344073ab921b3d13a3e98d3a44cb93536da180
Wesley Weisenberger <wesleynweisenberger@gmail.com>
2021-02-25 00:24:22 -0800

    holt sh1t this is fast now

--- bot.py
+++ bot.py
@@ -94,15 +94,20 @@ async def on_message(message):
     if message.author.bot:
         return
 
+
     collection = db[str(message.guild.id)]
-    try: 
+    try:
         counting_channel = bot.get_channel(collection.find_one({"channel" : "counting"}).get("snowflake"))
-    except: 
+    except AttributeError:
         counting_channel = None
 
     p = inflect.engine()
-    # check if someone counted incorrectly in the counting channel
+    # someone has counted in the counting channel
     if message.channel == counting_channel:
+        # store new message in db
+        # TODO: fix this, push isn't working correctly and I changed the db behavior 
+        # collection.update_one({"member" : message.author.id}, {"$push" : {"messages" : {"$each" : message.created_at}}})
+        collection.update_one({"counting_history" : {"$exists" : 1}}, {"$push" : {"counting_history" : [message.author.id, message.created_at]}}, upsert=True)
         latest_mesgs = [x for x in await counting_channel.history(limit=4).flatten() if "you've counted incorrectly" not in x.content.lower()]
         try: 
             latest_count = int(sub("[^0-9]", "", latest_mesgs[0].content))
@@ -169,108 +174,133 @@ async def link_error(ctx, error):
 async def leaderboard(ctx, *args):
     await leaderboard_print(ctx.channel, ctx.guild, *args)
 async def leaderboard_print(channel, guild, *args):
-    async with channel.typing():
-        collection = db[str(guild.id)]
-        try: 
-            counting_channel = bot.get_channel(collection.find_one({"channel" : "counting"}).get("snowflake"))
-            if not counting_channel:
-                await no_channel_set(channel, "counting")
-        except:
+    collection = db[str(guild.id)]
+    try: 
+        counting_channel = bot.get_channel(collection.find_one({"channel" : "counting"}).get("snowflake"))
+        if not counting_channel:
             await no_channel_set(channel, "counting")
-            return
+    except:
+        await no_channel_set(channel, "counting")
+        return
 
-        now = datetime.utcnow()
-        interval = 'Daily'
-        if len(args) >= 1:
-            if args[0] == 'daily':
-                td = timedelta(days=1)
-                interval = 'Daily'
-            elif args[0] == 'weekly':
-                td = timedelta(weeks=1)
-                interval = 'Weekly'
-            elif args[0] == 'monthly':
-                td = timedelta(weeks=4)
-                interval = 'Monthly'
-            elif args[0] == 'all':
-                oldest_mesg = await counting_channel.history(limit=1, oldest_first=True).flatten()
-                td = now - oldest_mesg[0].created_at    
-                interval = "All Time"
-            else:
-                await channel.send("Something went wrong. Try running this command again. You must specify **weekly**, **monthly**, or **all**, or just **#leaderboard** for all time.")
-                return
-        else:
+    now = datetime.utcnow()
+    interval = 'Daily'
+    if len(args) >= 1:
+        if args[0] == 'daily':
             td = timedelta(days=1)
             interval = 'Daily'
+        elif args[0] == 'weekly':
+            td = timedelta(weeks=1)
+            interval = 'Weekly'
+        elif args[0] == 'monthly':
+            td = timedelta(weeks=4)
+            interval = 'Monthly'
+        elif args[0] == 'all':
+            oldest_mesg = await counting_channel.history(limit=1, oldest_first=True).flatten()
+            td = now - oldest_mesg[0].created_at    
+            interval = "All Time"
+        else:
+            await channel.send("Something went wrong. Try running this command again. You must specify **weekly**, **monthly**, or **all**, or just **#leaderboard** for all time.")
+            return
+    else:
+        td = timedelta(days=1)
+        interval = 'Daily'
+
+    now = datetime.utcnow()
+    slowmode = counting_channel.slowmode_delay
+    counting_channel_history = collection.find_one({"counting_history" : {"$exists" : 1}}).get("counting_history")
+
+    ranks_and_efficiency = await calculate_member_stats(guild.members, None, counting_channel_history, slowmode, now - td)
+
+    embed = discord.Embed(color=0x3498db)
+    embed.add_field(name=f'{interval} Leaderboard 💯', value='___', inline=False)
+
+    for i in range(min(6, len(ranks_and_efficiency))):
+        place = i+1
+        if place == 1:
+            place = ":first_place:"
+        elif place == 2:
+            place = ":second_place:"
+        elif place == 3:
+            place = ":third_place:"
+        embed.add_field(name=f"***{place}*** - {ranks_and_efficiency[i][1].display_name}", value=f"{await efficiency_bar(ranks_and_efficiency[i][2])} ({ranks_and_efficiency[i][2]}%)", inline=False)
+    await channel.send(embed=embed)
+
+@bot.command()
+@commands.check(is_admin)
+async def index_messages(ctx):
+    collection = db[str(ctx.guild.id)]
+    # TODO: more efficient way to do the counting channel thing below
+    try: 
+        counting_channel = bot.get_channel(collection.find_one({"channel" : "counting"}).get("snowflake"))
+        if not counting_channel:
+            await no_channel_set(ctx.channel, "counting")
+            return
+    except:
+        await no_channel_set(ctx.channel, "counting")
+        return
     
-        now = datetime.utcnow()
-        slowmode = counting_channel.slowmode_delay
-        counting_channel_history = await counting_channel.history(limit=None).flatten()
-
-        ranks_and_efficiency = await calculate_member_stats(guild.members, None, counting_channel_history, slowmode, now - td)
-
-        embed = discord.Embed(color=0x3498db)
-        embed.add_field(name=f'{interval} Leaderboard 💯', value='___', inline=False)
-
-        for i in range(min(6, len(ranks_and_efficiency))):
-            place = i+1
-            if place == 1:
-                place = ":first_place:"
-            elif place == 2:
-                place = ":second_place:"
-            elif place == 3:
-                place = ":third_place:"
-            embed.add_field(name=f"***{place}*** - {ranks_and_efficiency[i][1].display_name}", value=f"{await efficiency_bar(ranks_and_efficiency[i][2])} ({ranks_and_efficiency[i][2]}%)", inline=False)
-        await channel.send(embed=embed)
+    mesg = await ctx.channel.send(f"Please wait... Indexing 0 messages in {counting_channel.mention}")
+    messages = []
+    counter = 0
+    collection.delete_many({"counting_history" : {"$exists" : 1}})
+    async for message in counting_channel.history(limit=None, oldest_first=True):
+        if message.author.bot:
+            pass
+        if counter % 10 == 0:
+            await mesg.edit(content=f"Please wait... Indexing {counter} messages in {counting_channel.mention}")
+        collection.update_one({"counting_history" : {"$exists" : 1}}, {"$push" : {"counting_history" : [message.author.id, message.created_at]}}, upsert=True)
+        counter += 1
+    await mesg.edit(content=f"Finished! Indexed {counter} messages in {counting_channel.mention}")
 
 
 
 @bot.command()
 async def stats(ctx, member : discord.Member = None):
     member = member or ctx.author 
-    async with ctx.channel.typing():
-        collection = db[str(ctx.guild.id)]
-        try: 
-            counting_channel = bot.get_channel(collection.find_one({"channel" : "counting"}).get("snowflake"))
-            if not counting_channel:
-                await no_channel_set(ctx.channel, "counting")
-                return
-        except:
+    collection = db[str(ctx.guild.id)]
+    try: 
+        counting_channel = bot.get_channel(collection.find_one({"channel" : "counting"}).get("snowflake"))
+        if not counting_channel:
             await no_channel_set(ctx.channel, "counting")
             return
+    except:
+        await no_channel_set(ctx.channel, "counting")
+        return
 
-        now = datetime.utcnow()
-        slowmode = counting_channel.slowmode_delay
-        counting_channel_history = await counting_channel.history(limit=None, oldest_first=True).flatten()
+    now = datetime.utcnow()
+    slowmode = counting_channel.slowmode_delay
+    counting_channel_history = collection.find_one({"counting_history" : {"$exists" : 1}}).get("counting_history")
 
-        # returns in format: [member, rank, efficiency]
-        daily_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(days=1))
-        prev_daily_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(days=2), now - timedelta(days=1))
-        weekly_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(weeks=1))
-        prev_weekly_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(weeks=2), now - timedelta(weeks=1))
+    # returns in format: [member, rank, efficiency]
+    daily_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(days=1))
+    prev_daily_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(days=2), now - timedelta(days=1))
+    weekly_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(weeks=1))
+    prev_weekly_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(weeks=2), now - timedelta(weeks=1))
 
-        monthly_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(weeks=4))
-        prev_monthly_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(weeks=8), now - timedelta(weeks=4))
+    monthly_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(weeks=4))
+    prev_monthly_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(weeks=8), now - timedelta(weeks=4))
 
-        oldest_time = counting_channel_history[0].created_at
-        all_time = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, oldest_time)
+    oldest_time = counting_channel_history[0][1]
+    all_time = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, oldest_time)
 
-        embed = discord.Embed(color=member.color)
-        embed.set_author(name=member.name+"'s stats", icon_url=member.avatar_url)
-        embed.set_footer(text="Counted a total of " + str(all_time[3]) + " times.")
+    embed = discord.Embed(color=member.color)
+    embed.set_author(name=member.name+"'s stats", icon_url=member.avatar_url)
+    embed.set_footer(text="Counted a total of " + str(all_time[3]) + " times.")
 
-        daily_direction = 'Up' if daily_stats[2] > prev_daily_stats[2] else 'Down'
-        daily_bar = await efficiency_bar(daily_stats[2]) + f" ({daily_stats[2]}%)"+f"\n```{daily_direction} {abs(round(daily_stats[2]-prev_daily_stats[2], 1))}% from yesterday```"
-        embed.add_field(name="Daily - Ranked #"+str(daily_stats[1]), value=daily_bar)
+    daily_direction = 'Up' if daily_stats[2] > prev_daily_stats[2] else 'Down'
+    daily_bar = await efficiency_bar(daily_stats[2]) + f" ({daily_stats[2]}%)"+f"\n```{daily_direction} {abs(round(daily_stats[2]-prev_daily_stats[2], 1))}% from yesterday```"
+    embed.add_field(name="Daily - Ranked #"+str(daily_stats[1]), value=daily_bar)
 
-        weekly_direction = 'Up' if weekly_stats[2] > prev_weekly_stats[2] else 'Down'
-        weekly_bar = await efficiency_bar(weekly_stats[2]) + f" ({weekly_stats[2]}%)"+f"\n```{weekly_direction} {abs(round(weekly_stats[2]-prev_weekly_stats[2], 1))}% from last week```"
-        embed.add_field(name='Weekly - Ranked #'+str(weekly_stats[1]), value=weekly_bar)
+    weekly_direction = 'Up' if weekly_stats[2] > prev_weekly_stats[2] else 'Down'
+    weekly_bar = await efficiency_bar(weekly_stats[2]) + f" ({weekly_stats[2]}%)"+f"\n```{weekly_direction} {abs(round(weekly_stats[2]-prev_weekly_stats[2], 1))}% from last week```"
+    embed.add_field(name='Weekly - Ranked #'+str(weekly_stats[1]), value=weekly_bar)
 
-        monthly_direction = 'Up' if monthly_stats[2] > prev_monthly_stats[2] else 'Down'
-        monthly_bar = await efficiency_bar(monthly_stats[2]) + f" ({monthly_stats[2]}%)"+f"\n```{monthly_direction} {abs(round( monthly_stats[2]-prev_monthly_stats[2], 1))}% from last month```"
-        embed.add_field(name='Monthly - Ranked #'+str(monthly_stats[1]), value=monthly_bar)
+    monthly_direction = 'Up' if monthly_stats[2] > prev_monthly_stats[2] else 'Down'
+    monthly_bar = await efficiency_bar(monthly_stats[2]) + f" ({monthly_stats[2]}%)"+f"\n```{monthly_direction} {abs(round( monthly_stats[2]-prev_monthly_stats[2], 1))}% from last month```"
+    embed.add_field(name='Monthly - Ranked #'+str(monthly_stats[1]), value=monthly_bar)
 
-        await ctx.send(embed=embed)
+    await ctx.send(embed=embed)
 @stats.error 
 async def stats_error(ctx, error):
     if isinstance(error, commands.BadArgument):
@@ -312,7 +342,7 @@ async def calculate_member_stats(members, req_member, channel_history, slowmode_
     if before is None:
         before = datetime.utcnow()
 
-    channel_history = [x for x in channel_history if x.created_at > after and x.created_at < before]
+    channel_history = [x[0] for x in channel_history if x[1] > after and x[1] < before]
     # .slowmode_delay is in seconds
     possible_counts_interval = round((before - after).total_seconds() / slowmode_delay)
 
@@ -320,7 +350,7 @@ async def calculate_member_stats(members, req_member, channel_history, slowmode_
     for member in members:
         counter = 0
         for message in channel_history:
-            if message.author == member:
+            if message == member.id:
                 counter +=1
         stats.append([member, round(counter / possible_counts_interval * 100, 2), counter])
 
