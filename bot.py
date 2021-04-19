fd810ccc7b25619608ee5c8453710032d7f656b9
Wesley Weisenberger <wesleynweisenberger@gmail.com>
2021-04-19 01:22:00 -0700

    possibly the last commit.

--- bot.py
+++ bot.py
@@ -1,51 +1,37 @@
 import discord
-import logging 
-import os
-import json
-import asyncio
-import inflect
-import textwrap
-import traceback
-from re import sub
-from emoji import demojize
-from random import choice, random
-from datetime import datetime, timedelta
 from discord.utils import get
 from discord.ext import commands, tasks
+import asyncio, logging
+import os, json, textwrap, traceback 
+from re import sub
+from random import choice, random
+from datetime import datetime, timedelta
 from pymongo import MongoClient
 from word2number import w2n
 from dotenv import load_dotenv
 load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
-
 token = os.environ.get('DISCORDTOKEN')
 intents = discord.Intents().default()
 intents.members = True
-
-bot = commands.Bot(command_prefix='$', intents=intents)
-
+bot = commands.Bot(command_prefix='$', intents=intents, help_command=None)
 db_client = MongoClient('localhost',27017)
 db = db_client["botsky"]
 
-with open('questions.json') as f:
-    questions = json.load(f)
 with open('activities.json') as f:
     activities = json.load(f)
 
-## EVENTS
+### TASKS / EVENTS
 @bot.event
 async def on_ready():
     logging.info(f'{bot.user.name} has connected to Discord!')
     change_presense.start()
     weekly_leaderboard.start()
-    floppa_friday.start()
 
 async def is_admin(ctx):
     return ctx.author.guild_permissions.administrator or ctx.author.id == 184880932476420097
 
-
-### TASKS
 @tasks.loop(minutes=15)
 async def change_presense():
     activity = discord.Game(choice(activities))
@@ -53,35 +39,14 @@ async def change_presense():
 
 @tasks.loop(hours=24*7)
 async def weekly_leaderboard():  
-    # trigger every synday at 20:00 PST --- server is now in PST
-    # there has to be a more efficient way to do this
     await sleep_until_hour(20)
     while datetime.now().weekday() != 6:
         await asyncio.sleep(24 * 60 * 60)
+    coll = db['guild_parameters']
     for guild in bot.guilds:
-        collection = db[str(guild.id)]
-        try: 
-            announcements_channel = bot.get_channel(collection.find_one({"channel" : "announcements"}).get("snowflake"))
-        except: 
-            return
-        if not announcements_channel: return
-        await leaderboard_print(announcements_channel, guild, 'weekly')
-
-@tasks.loop(hours=24*7)
-async def floppa_friday():
-    # there has to be a more efficient way to do this
-    # trigger every friday at 13:00 PST
-    await sleep_until_hour(9)
-    while datetime.now().weekday() != 4:
-        await asyncio.sleep(24 * 60 * 60)
-    for guild in bot.guilds:
-        collection = db[str(guild.id)]
-        try: 
-            announcements_channel = bot.get_channel(collection.find_one({"channel" : "announcements"}).get("snowflake"))
-            print(announcements_channel)
-        except:
-            return 
-        await announcements_channel.send(file=discord.File('floppa_friday.mov'))
+        params = coll.find_one({"_id" : guild.id})
+        if params and "announcements" in params and bot.get_channel(params["announcements"]) and "weekly-leaderboard_disable" not in params:
+            await leaderboard_print(bot.get_channel(params["announcements"]), guild, 'weekly')
 
     
 
@@ -94,15 +59,13 @@ async def on_message(message):
     if message.author.bot:
         return
 
-    collection = db[str(message.guild.id)]
-    try:
-        counting_channel = bot.get_channel(collection.find_one({"channel" : "counting"}).get("snowflake"))
-    except AttributeError:
-        counting_channel = None
-
-    p = inflect.engine()
-    if message.channel == counting_channel:
-        collection.update_one({"counting_history" : {"$exists" : 1}}, {"$push" : {"counting_history" : [message.author.id, message.created_at]}}, upsert=True)
+    coll = db['guild_parameters']
+    counting_channel = await db_get_channel(message.guild.id, 'counting')
+    if not counting_channel:
+        return
+    if message.channel == counting_channel and "counting-errors_disable" not in coll.find_one({"_id" : message.guild.id}):
+        coll = db['guild_message_history']
+        coll.update_one({"_id" : message.guild.id}, {"$push" : {"messages" : [message.author.id, message.created_at]}}, upsert=True)
         latest_mesgs = [x.content for x in await counting_channel.history(limit=5).flatten() if "you've counted incorrectly" not in x.content.lower()]
 
         async def convert(n : str) -> int:
@@ -130,26 +93,16 @@ async def on_message(message):
                     return 0
 
         if await convert(latest_mesgs[0]) != await convert(latest_mesgs[1]) + 1:
-            mistakes = (collection.find_one({"member" : message.author.id}) or {0:0}).get("mistakes", 0) + 1
-            collection.update_one({"member" : message.author.id}, {"$inc" : {"mistakes" : 1}}, upsert=True)
-            mesg = message.author.mention + " You've counted incorrectly. This is your " + p.ordinal(mistakes) + " mistake. Please fix your number."
+            mesg = message.author.mention + " You've counted incorrectly. Please fix your number."
             await counting_channel.send(mesg, delete_after=10)
-    else:
-        if 'tuesday' in message.content.lower():
-            await message.channel.send(file=discord.File('tueday.png'))
-        # 2% chance of sending a random message
-        if random() < 0.005:
-            await message.channel.send(message.author.mention+" "+choice(questions))
+
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CheckFailure):
         await ctx.send("You don't have permission to run this command...")
 
-# TODO: possibly add on_raw_reaction_add to work for older messages
-@bot.event
-async def on_reaction_add(reaction, user):
-    if reaction.emoji == "⭐" and reaction.message.attachments:
-        await user.send(reaction.message.attachments[0].url)
+
+
 
 ### COMMANDS
 @bot.command()
@@ -157,13 +110,27 @@ async def ping(ctx):
     await ctx.send(f'Pong! **{round(bot.latency, 3)}ms**.')
 
 @bot.command()
+async def help(ctx):
+    embed=discord.Embed(title="⭐ botsky help ⭐", description="______", color=0x3498db)
+    embed.add_field(name="$stats [optional: @user]", value="Displays the counting stats of either the user who called the command or whoever is mentioned.", inline=False)
+    embed.add_field(name="$leaderboard [optional: daily/weekly/monthly/all]", value="Displays a 5 person leaderboard for the time period selected. If not time period is selected defaults to daily.", inline=False)
+    embed.add_field(name="$link [counting/announcements]", value="Can only be run by an admin. This needs to be run prior to running the $stats or $leaderboard command. Links a counting channel or an announcements channel to send the weekly leaderboard.", inline=False)
+    embed.add_field(name="$index-messages", value="Can only be run by an admin. This needs to be run prior to running the $stats or $leaderboard command. It may take a minute.", inline=False)
+    embed.add_field(name="$enable/$disable [counting-errors/weekly-leaderboard]", value="Can only be run by an admin. Enable/Disable errors when someone counts incorrectly in the counting channel or the weekly leaderboard.", inline=False)
+    embed.add_field(name="$feedback", value="Send feedback to Wesley, the creator of this bot.", inline=False)
+    embed.add_field(name="$length", value="Self explanatory.", inline=False)
+    embed.add_field(name="$ping", value="Self explanatory.", inline=False)
+    embed.add_field(name="______", value="https://github.com/wesleynw/botsky", inline=False)
+    await ctx.send(embed=embed)
+
+@bot.command()
 @commands.check(is_admin)
 async def link(ctx, category : str, channel : discord.TextChannel):
     channel_types = ['counting', 'announcements']
     if category not in channel_types:
         raise commands.BadArgument
-    collection = db[str(ctx.guild.id)]
-    collection.replace_one({"channel" : category}, {"channel" : category, "snowflake" : channel.id}, upsert=True)
+    coll = db['guild_parameters']
+    coll.update_one({"_id" : ctx.guild.id}, {"$set" : {category : channel.id}}, upsert=True)
     await ctx.send(f"The {category} channel has been set to {channel.mention}")
 @link.error
 async def link_error(ctx, error):
@@ -174,13 +141,10 @@ async def link_error(ctx, error):
 async def leaderboard(ctx, *args):
     await leaderboard_print(ctx.channel, ctx.guild, *args)
 async def leaderboard_print(channel, guild, *args):
-    collection = db[str(guild.id)]
-    try: 
-        counting_channel = bot.get_channel(collection.find_one({"channel" : "counting"}).get("snowflake"))
-        if not counting_channel:
-            await no_channel_set(channel, "counting")
-    except:
-        await no_channel_set(channel, "counting")
+    coll = db['guild_parameters']
+    counting_channel = await db_get_channel(guild.id, 'counting')
+    if not counting_channel:
+        await no_channel_set(channel, 'counting')
         return
 
     now = datetime.utcnow()
@@ -208,14 +172,18 @@ async def leaderboard_print(channel, guild, *args):
 
     now = datetime.utcnow()
     slowmode = counting_channel.slowmode_delay
-    counting_channel_history = collection.find_one({"counting_history" : {"$exists" : 1}}).get("counting_history")
+    coll = db['guild_message_history']
+    params = coll.find_one({"_id" : guild.id})
+    if not (params and params.get('messages')):
+        await channel.send("Please run **$index-messages** so I can view your past counting channel messages.")
+    counting_channel_history = params.get('messages')
 
     ranks_and_efficiency = await calculate_member_stats(guild.members, None, counting_channel_history, slowmode, now - td)
 
     embed = discord.Embed(color=0x3498db)
     embed.add_field(name=f'{interval} Leaderboard 💯', value='___', inline=False)
 
-    for i in range(min(6, len(ranks_and_efficiency))):
+    for i in range(min(5, len(ranks_and_efficiency))):
         place = i+1
         if place == 1:
             place = ":first_place:"
@@ -225,50 +193,50 @@ async def leaderboard_print(channel, guild, *args):
             place = ":third_place:"
         embed.add_field(name=f"***{place}*** - {ranks_and_efficiency[i][1].display_name}", value=f"{await efficiency_bar(ranks_and_efficiency[i][2])} ({ranks_and_efficiency[i][2]}%)", inline=False)
     await channel.send(embed=embed)
+    coll = db['guild_parameters']
+    for guild in bot.guilds:
+        opts = coll.find_one({"_id" : guild.id})
+        if not (opts and "announcements" in opts  and bot.get_channel(opts["announcements"])):
+            await no_channel_set(channel, 'announcements')
 
-@bot.command()
+@bot.command(name='index-messages')
 @commands.check(is_admin)
 async def index_messages(ctx):
-    collection = db[str(ctx.guild.id)]
-    # TODO: more efficient way to do the counting channel thing below
-    try: 
-        counting_channel = bot.get_channel(collection.find_one({"channel" : "counting"}).get("snowflake"))
-        if not counting_channel:
-            await no_channel_set(ctx.channel, "counting")
-            return
-    except:
-        await no_channel_set(ctx.channel, "counting")
+    counting_channel = await db_get_channel(ctx.guild.id, 'counting')
+    if not counting_channel:
+        await no_channel_set(ctx.channel, 'counting')
         return
     
     mesg = await ctx.channel.send(f"Please wait... Indexing 0 messages in {counting_channel.mention}")
     counter = 0
-    collection.delete_many({"counting_history" : {"$exists" : 1}})
+    coll = db["guild_message_history"]
+    coll.delete_many({"_id" : ctx.guild.id})
     async for message in counting_channel.history(limit=None, oldest_first=True):
-        if message.author.bot:
-            pass
+        # if message.author.bot:
+        #     pass
         if counter % 10 == 0:
             await mesg.edit(content=f"Please wait... Indexing {counter} messages in {counting_channel.mention}")
-        collection.update_one({"counting_history" : {"$exists" : 1}}, {"$push" : {"counting_history" : [message.author.id, message.created_at]}}, upsert=True)
+        coll.update_one({"_id" : ctx.guild.id}, {"$push" : {"messages" : [message.author.id, message.created_at]}}, upsert=True)
         counter += 1
     await mesg.edit(content=f"Finished! Indexed {counter} messages in {counting_channel.mention}")
 
 @bot.command()
 async def stats(ctx, member : discord.Member = None):
     member = member or ctx.author 
-    collection = db[str(ctx.guild.id)]
-    try: 
-        counting_channel = bot.get_channel(collection.find_one({"channel" : "counting"}).get("snowflake"))
-        if not counting_channel:
-            await no_channel_set(ctx.channel, "counting")
-            return
-    except:
-        await no_channel_set(ctx.channel, "counting")
+    coll = db['guild_parameters']
+    counting_channel = await db_get_channel(ctx.guild.id, 'counting')
+    if not counting_channel:
+        await no_channel_set(ctx.channel, 'counting')
         return
+    slowmode = counting_channel.slowmode_delay
+
+    coll = db['guild_message_history']
+    params = coll.find_one({"_id" : ctx.guild.id})
+    if not (params and params.get('messages')):
+        await ctx.send("Please run **$index-messages** so I can view your past counting channel messages.")
+    counting_channel_history = params.get('messages')
 
     now = datetime.utcnow()
-    slowmode = counting_channel.slowmode_delay
-    counting_channel_history = collection.find_one({"counting_history" : {"$exists" : 1}}).get("counting_history")
-
     # returns in format: [member, rank, efficiency]
     daily_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(days=1))
     prev_daily_stats = await calculate_member_stats(ctx.guild.members, member, counting_channel_history, slowmode, now - timedelta(days=2), now - timedelta(days=1))
@@ -299,6 +267,11 @@ async def stats(ctx, member : discord.Member = None):
     embed.add_field(name='Monthly - Ranked #'+str(monthly_stats[1]), value=monthly_bar)
 
     await ctx.send(embed=embed)
+    coll = db['guild_parameters']
+    for guild in bot.guilds:
+        opts = coll.find_one({"_id" : guild.id})
+        if not(opts and "announcements" in opts  and bot.get_channel(opts["announcements"])):
+            await no_channel_set(ctx.channel, 'announcements')
 @stats.error 
 async def stats_error(ctx, error):
     if isinstance(error, commands.BadArgument):
@@ -308,23 +281,64 @@ async def stats_error(ctx, error):
 async def length(ctx, member : discord.Member = None):
     member = member or ctx.author
     length = round(member.id / 10**17, 1)
-
     if member.id == 184880932476420097:
         length = 7
     elif member.id == 495824425711828993:
         length = 11.1
-
     await ctx.send(f"{member.mention}'s cock length is {length} inches.")
-        
-        
+
+@bot.command()
+@commands.check(is_admin)
+async def enable(ctx, option):
+    if option in ["weekly-leaderboard", "counting-errors"]:
+        coll = db["guild_parameters"]
+        coll.update_one({"_id" : ctx.guild.id}, {"$unset" : {option + "_disable" : ""}})
+    else:
+        raise commands.BadArgument
+    await ctx.send('I have enabled for '+option)
+@enable.error
+async def enable_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send(f"There was an error, please try again... You can use the enable command on **weekly-leaderboard** or **counting-errors**.")
+
+@bot.command()
+@commands.check(is_admin)
+async def disable(ctx, option):
+    if option in ["weekly-leaderboard", "counting-errors"]:
+        coll = db['guild_parameters']
+        coll.update_one({"_id" : ctx.guild.id}, {"$set" : {option + "_disable" : True}}, upsert=True)
+    else:
+        raise commands.BadArgument
+    await ctx.send('I have disabled '+option)
+@disable.error
+async def disable_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send(f"There was an error, please try again... You can use the disable command on **weekly-leaderboard** or **counting-errors**.")
+
+@bot.command()
+async def feedback(ctx):
+    wesley = bot.get_user(184880932476420097)
+    await wesley.send(ctx.message.content)
+
+
 
 
 ### FUNCTIONS
-async def no_channel_set(channel, category):
+async def db_get_channel(guild_id : int, channel_type : str) -> discord.TextChannel:
+    """Return discord.TextChannel instance of channel_type if it exists in the db."""
+    coll = db['guild_parameters']
+    params = coll.find_one({"_id" : guild_id})
+    if params and channel_type in params and bot.get_channel(params[channel_type]):
+        return bot.get_channel(params[channel_type])
+
+async def set_announcements_channel_reminder(channel):
+    await channel.send('By the way, you should link an announcements channel using **$link announcements *#channel*')
+async def no_channel_set(mesg_channel : discord.TextChannel, category : str):
+    """Send a message to mesg_channel announcing that a certain channel must be set."""
     if category == "counting":
-        await channel.send('You must set a counting channel using **$link counting** ***#channel***.')
+        await mesg_channel.send('You must set a counting channel using **$link counting** ***#channel***.')
     elif category == "announcements":
-        await channel.send('You must set an announcements channel using **$link announcements** ***#channel***.')
+        await mesg_channel.send('You must set an announcements channel using **$link announcements** ***#channel***.')
 
 async def sleep_until_hour(hour : int):
     now = datetime.now()
